dc5fdd7e20415901d5ac410e052246b4bc7c8fbf
lucasdvl01 <lucas.2008.dovale@gmail.com>
2026-08-05 17:44:40 -0300

    terceiro commit
    teste de insert

--- fifa.php
+++ fifa.php
@@ -7,6 +7,26 @@
 </head>
 <body>
     
+<h2>Cadastrar Usuário</h2>
+
+<form action="cadastrar.php" method="POST">
+
+    <label>Nome:</label><br>
+    <input type="text" name="nome"><br><br>
+
+    <label>Email:</label><br>
+    <input type="email" name="email"><br><br>
+
+    <label>Senha:</label><br>
+    <input type="password" name="senha"><br><br>
+
+    <label>Tipo:</label><br>
+    <input type="text" name="tipo_usuario"><br><br>
+
+    <button type="submit">Cadastrar</button>
+
+</form>
+
 </body>
 </html>
 <?php
@@ -22,6 +42,23 @@ if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
 
+include "conexao.php";
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+$tipo = $_POST["tipo_usuario"];
+
+$sql = "INSERT INTO USUARIO (tipo_usuario, senha, email, nome)
+        VALUES ('$tipo', '$senha', '$email', '$nome')";
+
+if ($conn->query($sql)) {
+    echo "Usuário cadastrado com sucesso!";
+} else {
+    echo "Erro: " . $conn->error;
+}
+
+$conn->close();
 
 
 
