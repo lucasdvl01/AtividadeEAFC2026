6777a217900511126c56ea069ae7efc8f88c2f6a
lucasdvl01 <lucas.2008.dovale@gmail.com>
2026-08-05 17:24:55 -0300

    segundo commit cadastro do banco

--- fifa.php
+++ fifa.php
@@ -11,7 +11,16 @@
 </html>
 <?php
 
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "fifadados";
 
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
 
 
 
